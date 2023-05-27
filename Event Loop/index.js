3ef5c14801c1b1632ac456328096baf0545bce9f
Ваше Имя <you@example.com>
2023-05-27 17:56:39 +0400

    event loop

---
+++ Event Loop/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Event loop
+
+console.log(1);
+
+setTimeout(() => {
+  console.log(2);
+}, 0);
+
+const myPromise = new Promise((resolve, reject) => {
+  console.log(3);
+  resolve(4);
+}).then((value) => console.log(value));
+
+console.log(5);
+
+// отсчет 3 сек
